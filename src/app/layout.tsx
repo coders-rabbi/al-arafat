8e01b91680d8dcfce5ca526bf6e98d12febbea7d
codersrabbi <pcrabbi2020@gmail.com>
2026-04-04 22:32:50 +0600

    Al Arafa Project setup and install all the dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Providers from "@/lib/Providers/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <Providers>
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </body>
+      </html>
+    </Providers>
   );
 }
---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,36 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#008e48",
+    },
+    secondary: {
+      main: "#333333",
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          padding: "8px 24px",
+        },
+      },
+    },
+    MuiContainer: {
+      defaultProps: {
+        maxWidth: "lg",
+      },
+    },
+  },
+  typography: {
+    body1: {
+      color: "#0F2920",
+    },
+  },
+});
+
+theme.shadows[1] = "0px 5px 22px lightgray";
