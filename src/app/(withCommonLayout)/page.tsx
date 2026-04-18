b757e57765dc02446198bc08e12f3c0df5866592
codersrabbi <pcrabbi2020@gmail.com>
2026-04-18 22:13:48 +0600

    try to fixed vercel error

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,9 @@
-import BlogCard from "@/components/ui/HomePage/BlogCard/blogCard";
-import DonationForm from "@/components/ui/HomePage/DonationForm/donationForm";
-import { EventCarousel } from "@/components/ui/HomePage/EventCarousel/eventCarousel";
+import BlogCard from "@/components/ui/HomePage/blogCard/blogCard";
+import DonationForm from "@/components/ui/HomePage/donationForm/donationForm";
+import { EventCarousel } from "@/components/ui/HomePage/eventCarousel/eventCarousel";
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
 import NewsLetter from "@/components/ui/HomePage/newsLetter/newsLetter";
-import UmmahVision from "@/components/ui/HomePage/UmmahVision/UmmahVision";
+import UmmahVision from "@/components/ui/HomePage/ummahVision/ummahVision";
 
 const HomePage = () => {
   return (
